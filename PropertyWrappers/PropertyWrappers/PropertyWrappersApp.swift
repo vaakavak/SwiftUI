507659ae5e97ba24ce1161f6ffa174366be33f0b
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-06-21 09:56:32 +0400

    create new project
    Project to study property wrappers

---
+++ PropertyWrappers/PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Admin on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
