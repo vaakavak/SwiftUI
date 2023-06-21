c4bce540ff1b04e2d345581ae2acc78cea4b7a1f
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-06-21 10:07:57 +0400

    @State
    Создал кнопку, при нажатии на которую изменяется значение,

--- PropertyWrappers/PropertyWrappers/ContentView.swift
+++ PropertyWrappers/PropertyWrappers/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var counter = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Состояние \(counter)")
+            Button("Увеличить состояние") {
+                counter += 1
+            }
         }
-        .padding()
     }
 }
 
