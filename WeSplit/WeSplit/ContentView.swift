6d60675dc4c3cd60053fb458bfc3a91011e7d416
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-06-22 16:59:13 +0400

    finished create interface

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,19 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Гарри", "Гермиона", "Рон"]
-    @State private var selectedStudents = "Гарри"
+    @State private var checkAmount = 0.0
+    @State private  var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25, 30]
     
     
     var body: some View {
         NavigationView {
             Form {
-                Picker("Выберите студента", selection: $selectedStudents) {
-                    ForEach (students, id: \.self) {
-                        Text("\($0)")
+                Section {
+                    TextField("Сумма чека", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Количество людей", selection: $numberOfPeople) {
+                        ForEach (2..<100) {
+                            Text("\($0)  человек")
+                        }
                     }
                 }
+                Section {
+                    Picker("Кол-во чаевых в %", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text(($0), format: .percent) //percent - преобразуем в проценты
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            header: {
+                    Text("Выберите процент чевых")
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
