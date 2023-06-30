971d5fe14185736a56aadfdfc8353201043232d5
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-06-30 10:14:25 +0400

    create new progect GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Admin on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
