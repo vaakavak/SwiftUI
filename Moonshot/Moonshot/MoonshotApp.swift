9f5df4e820394dcc29ca202e6142c5e6223b8164
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-09-11 21:31:42 +0400

    new project Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Евгений Шилов on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
