df717b618f649c6c6fd8f2d08ea331c28a60df9b
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-09-13 09:17:14 +0400

    Moonshot project ScrollView

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+        ScrollView {
+            VStack (spacing: 30) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
+            }
         }
             
     }
